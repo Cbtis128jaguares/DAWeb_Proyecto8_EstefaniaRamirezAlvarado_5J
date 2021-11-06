fa8130525fd0cd31a09405c224fafcc5e929c752
Estefania Ramirez Alvarado <85502117+Cbtis128jaguares@users.noreply.github.com>
2021-11-05 21:13:09 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# DAWeb_Proyecto8_EstefaniaRamirezAlvarado_5J
+# ProyectoEstefania_UII_Act8
+Práctica 8 Proyecto DAWeb Ramirez Alvarado 5J - Página web de prueba hecha con PHP para la práctica 8 de la materia de DAWeb Unidad II
+
+Estefania Ramirez Alvarado 5J
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="estilos.css">
+<link rel="icon" href="img\logogasolina.png">
+<head>
+	<meta charset="UTF-8">
+    <title>Estefania Ramirez Alvarado 5J</title>
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<img src="img\gasolina.jpg" width="60" height="60">
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="empleado.php">Empleado</a></li>
+					<li><a href="ventas.php">Ventas</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
